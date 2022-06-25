dce0541cdb03bd1184b33a667e49c01b23fa77eb
Preksler <preksler@gmail.com>
2022-06-26 01:39:06 +0300

    tast 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
 import { Profile } from "./Profile/Profile";
-import user from "../data/user.json"
+import user from "../data/user.json";
+import data from "../data/data.json";
+import { Statistics } from "./Statistics/Statistics";
 
 export const App = () => {
   return (
+    <>
     <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats} />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </>
   );
 };
